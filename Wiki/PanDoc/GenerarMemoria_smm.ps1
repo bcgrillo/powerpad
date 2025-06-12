2a7fdd6e1f45ea3ba4ca07f8f42f2a429f6da051
Bruno 👨‍💻 <bcgrillo@users.noreply.github.com>
2025-06-13 00:15:23 +0200

    wiki

---
+++ Wiki/PanDoc/GenerarMemoria_smm.ps1
@@ -0,0 +1,42 @@
+$outputFile = "PanDoc/MemoriaPowerPad.docx"
+$memoriaPath = "_ Memoria.md"
+$tempPath = "_ Memoria_temp.md"
+
+cd ..
+
+if (Test-Path $outputFile) {
+    try {
+        Remove-Item $outputFile -ErrorAction Stop
+    } catch {
+        Write-Error "No se puede borrar el archivo existente. Deteniendo el script."
+	cd PanDoc
+        exit
+    }
+}
+
+# Lee el contenido de Memoria.md en UTF-8
+$content = Get-Content -Path $memoriaPath -Raw -Encoding utf8
+
+# Funcion de reemplazo
+$pattern = '!\[\[(.+?)\]\]'
+$content = [System.Text.RegularExpressions.Regex]::Replace($content, $pattern, {
+    param($match)
+    $fileName = "$($match.Groups[1].Value).md"
+    $filePath = $fileName
+    if (Test-Path $filePath) {
+        # Leemos el archivo y nos aseguramos de quitar saltos de línea finales extra
+        (Get-Content -Path $filePath -Raw -Encoding utf8).TrimEnd()
+    }
+    else {
+        $match.Value
+    }
+})
+
+# Escribe el resultado en el archivo temporal en UTF-8
+Set-Content -Path $tempPath -Value $content -Encoding utf8
+
+pandoc $tempPath --toc --citeproc --bibliography=PanDoc/References.bib --csl PanDoc/Templates/ieee-with-url.csl -o $outputFile --reference-doc=PanDoc/Templates/article.docx
+
+Remove-Item $tempPath
+
+cd PanDoc
